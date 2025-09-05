987497254e2863c2d67854ab92f5bc318379ceea
camay718 <camay718@gmail.com>
2025-09-05 20:52:14 +0900

    Create firebase-config.js

---
+++ v2/js/config/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase設定（本番環境用）
+const firebaseConfig = {
+    apiKey: "AIzaSyBkcO6x_CcIdxfq1PHzwT46k1VA6sXTN_o", // ← 新しいAPIキー
+    authDomain: "ce-schedule-management.firebaseapp.com",
+    databaseURL: "https://ce-schedule-management-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ce-schedule-management",
+    storageBucket: "ce-schedule-management.firebasestorage.app",
+    messagingSenderId: "998140589230",
+    appId: "1:998140589230:web:6c8159f7c1242cd5aeb4d0"
+};
+
+export { firebaseConfig };
