596aaf570b0765f6cd679f33067c3c0e27911948
camay718 <camay718@gmail.com>
2025-09-06 00:18:56 +0900

    Update firebase-config.js

--- v2/js/core/firebase-config.js
+++ v2/js/core/firebase-config.js
@@ -7,15 +7,16 @@ if (typeof window.firebaseConfigV2 !== 'undefined') {
 } else {
     // Firebase設定
     window.firebaseConfigV2 = {
-        apiKey: "AIzaSyCRUvvs0OSz_9L9bXtqteVFIIze1OaZObE",
-        authDomain: "ce-scheduling-system-v2.firebaseapp.com",
-        databaseURL: "https://ce-scheduling-system-v2-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "ce-scheduling-system-v2",
-        storageBucket: "ce-scheduling-system-v2.appspot.com",
-        messagingSenderId: "288279598010",
-        appId: "1:288279598010:web:d545ee1d4d854513084383",
-        measurementId: "G-LSEEMJE2R0"
-    };
+        const firebaseConfig = {
+  apiKey: "AIzaSyCRUvvs0OSz_9L9bXtqteVFIIze1OaZObE",
+  authDomain: "ce-scheduling-system-v2.firebaseapp.com",
+  databaseURL: "https://ce-scheduling-system-v2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ce-scheduling-system-v2",
+  storageBucket: "ce-scheduling-system-v2.firebasestorage.app",
+  messagingSenderId: "288279598010",
+  appId: "1:288279598010:web:d545ee1d4d854513084383",
+  measurementId: "G-LSEEMJE2R0"
+};
 
     // V2用データルート
     window.DATA_ROOT = 'ceScheduleV2';
