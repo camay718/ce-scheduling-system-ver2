593e0663e4dccad889774740f8681d4b35fb69fb
camay718 <camay718@gmail.com>
2025-09-06 00:09:21 +0900

    Update firebase-config.js

--- v2/js/config/firebase-config.js
+++ v2/js/config/firebase-config.js
@@ -6,4 +6,4 @@ window.firebaseConfig = {
     storageBucket: "ce-schedule-management.firebasestorage.app",
     messagingSenderId: "998140589230",
     appId: "1:998140589230:web:6c8159f7c1242cd5aeb4d0"
-};
+}
